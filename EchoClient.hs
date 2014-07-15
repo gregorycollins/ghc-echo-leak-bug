3939b4f418e8908cad3735089b478e29d4095bb4
Gregory Collins <greg@gregorycollins.net>
2014-07-15 22:42:01 +0200

    Rework test case to remove possible sources of error -- client is leaking.

--- EchoClient.hs
+++ EchoClient.hs
@@ -1,53 +1,80 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main
     (
       main
     ) where
 
-import           Control.Concurrent       (forkIO, threadDelay)
-import           Control.Concurrent.Async (async, waitCatch)
-import           Control.Exception        (finally)
-import           Control.Monad            (forever, replicateM, void)
-import           Data.ByteString          (hGetLine)
-import           Data.ByteString.Char8    (hPutStrLn)
-import           Data.IORef               (IORef, atomicModifyIORef', newIORef,
-                                           readIORef)
-import           Network                  (PortID (PortNumber), connectTo)
-import           System.Environment       (getArgs)
-import           System.IO                (BufferMode (LineBuffering),
-                                           hSetBuffering)
+import           Control.Concurrent
+import qualified Control.Exception         as E
+import           Control.Monad             (replicateM, void)
+import           Data.ByteString.Char8     ()
+import           Data.IORef                (IORef, atomicModifyIORef', newIORef,
+                                            readIORef)
+import           Network.Socket
+import qualified Network.Socket.ByteString as N
+import           System.Environment        (getArgs)
 
 main :: IO ()
 main = do
     [ip, port, spawnCount, pingFreq] <- getArgs
     count <- newIORef (0 :: Int)
-    void $ forkIO $ watcher count
-    asyncs <- replicateM (read spawnCount) $
-                async $ echoClient ip (read port) (read pingFreq) count
-    mapM_ waitCatch asyncs
+    go ip (read port) (read spawnCount) (read pingFreq) count
+  where
+    go ip port spawnCount pingFreq countRef = E.bracket start end wait
+      where
+        start = do
+            w_tid   <- forkIO $ watcher countRef
+            clients <- replicateM spawnCount spawnClient
+            return (w_tid, clients)
+
+        end (t, xs) = eatExceptions (killThread t) >> mapM_ killClient xs
+
+        wait = mapM_ waitClient . snd
+
+        spawnClient = E.mask $ \restore -> do
+            mv <- newEmptyMVar
+            tid <- restore (echoClient ip port pingFreq countRef) `forkFinally` const (putMVar mv ())
+            return (tid, mv)
+
+        killClient = eatExceptions . killThread . fst
+
+        waitClient = takeMVar . snd
+
+        eatExceptions m = m `E.catch` \(_ :: E.SomeException) -> return ()
+
 
 echoClient :: String -> Int -> Float -> IORef Int -> IO ()
 echoClient host port pingFreq count = do
-    h <- connectTo host (PortNumber (fromIntegral port))
-    hSetBuffering h LineBuffering
+    sock <- socket AF_INET Stream defaultProtocol
+    (ainfo:_) <- getAddrInfo hints (Just host) (Just $ show port)
+    let addr = addrAddress ainfo
+    connect sock addr
     incRef count
-    finally (loop h) (decRef count)
+    E.finally (loop sock) (decRef count)
   where
-    loop h = do
-        hPutStrLn h "PING"
-        _ <- hGetLine h
-        threadDelay (round $ pingFreq*1000000)
-        loop h
+    hints = Just $ defaultHints {addrFlags = [AI_NUMERICSERV]}
+    loop sock = let go = do N.sendAll sock "PING\n"
+                            _ <- N.recv sock 128
+                            threadDelay (round $ pingFreq*1000000)
+                            go
+                in go
+
 
 watcher :: IORef Int -> IO ()
-watcher i = forever $ do
-  count <- readIORef i
-  putStrLn $ "Clients Connected: " ++ (show count)
-  threadDelay (5*1000000)
+watcher i = go
+  where
+    go = do
+        count <- readIORef i
+        putStrLn $ "Clients Connected: " ++ (show count)
+        threadDelay (5*1000000)
+        go
+
 
 incRef :: Num a => IORef a -> IO ()
 incRef ref = void $ atomicModifyIORef' ref (\x -> (x+1, ()))
+
 
 decRef :: Num a => IORef a -> IO ()
 decRef ref = void $ atomicModifyIORef' ref (\x -> (x-1, ()))
--- EchoServer.hs
+++ EchoServer.hs
@@ -3,33 +3,48 @@ module Main
       main
     ) where
 
-import           Control.Concurrent    (forkFinally)
-import           Control.Monad         (forever)
-import           Data.ByteString       (hGetLine)
-import           Data.ByteString.Char8 (hPutStrLn)
-import           Network               (PortID (PortNumber), accept, listenOn,
-                                        withSocketsDo)
-import           System.IO             (BufferMode (LineBuffering), Handle,
-                                        hClose, hSetBuffering)
-import           Text.Printf           (printf)
+import           Control.Concurrent
+import qualified Control.Exception         as E
+import           Control.Monad             (forever)
+import           Data.ByteString           (hGetLine)
+import           Data.ByteString.Char8     (hPutStrLn)
+import           Network                   (withSocketsDo)
+import           Network.Socket
+import qualified Network.Socket.ByteString as N
+import           System.IO                 (BufferMode (LineBuffering), Handle,
+                                            hClose, hSetBuffering)
+import           Text.Printf               (printf)
 
 port :: Int
 port = 8080
 
 main :: IO ()
 main = withSocketsDo $ do
-    sock <- listenOn (PortNumber (fromIntegral port))
-    printf "Listening on port %d\n" port
-    forever $ do
-        (handle, _, _) <- accept sock
-        forkFinally (echo handle) (\_ -> hClose handle)
+    mv <- newEmptyMVar
+    E.bracket (forkFinally mainThread (const $ putMVar mv ()))
+              killThread
+              (const $ takeMVar mv)
+  where
+    mainThread = do
+        sock <- socket AF_INET Stream defaultProtocol
+        (ainfo:_) <- getAddrInfo hints (Just "127.0.0.1") (Just $ show port)
+        let addr = addrAddress ainfo
+        bind sock addr
+        listen sock 2048
+        printf "Listening on port %d\n" port
+        let loop = do (csock, _) <- accept sock
+                      forkFinally (echo csock) (const $ sClose csock)
+                      loop
+        loop
 
-echo :: Handle -> IO ()
-echo h = do
-    hSetBuffering h LineBuffering
+    hints = Just $ defaultHints {addrFlags = [AI_NUMERICHOST, AI_NUMERICSERV]}
+
+
+echo :: Socket -> IO ()
+echo sock = do
     loop
   where
     loop = do
-        line <- hGetLine h
-        hPutStrLn h line
+        line <- N.recv sock 128
+        N.sendAll sock line
         loop
